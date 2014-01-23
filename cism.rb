efdf26595f26e199dcbc3890cc5dc727a19e4888
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-23 12:54:38 -0700

    this is some data and stuff for the presentation

---
+++ cism.rb
@@ -0,0 +1,38 @@
+class Raindrops
+
+  def convert(i)
+    plingplangplong.map do |factor, sound|
+        sound if i % factor == 0
+      end
+    plingplangplong.any? ? plingplangplong.join : i.to_s
+  end
+
+  def plingplangplong
+    %w(
+         'Pling', 
+       'Plang',
+         'Plong'
+         )
+  end
+
+end
+
+
+class Raindrops
+
+  def convert(i)
+    plingplangplong.map do |factor, sound|
+        sound if i % factor == 0
+      end
+    plingplangplong.any? ? plingplangplong.join : i.to_s
+  end
+
+  def plingplangplong
+    %w(
+         'Pling', 
+       'Plang',
+         'Plong'
+         )
+  end
+
+end
