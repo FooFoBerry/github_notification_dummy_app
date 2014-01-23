57a93d7e52112f695fd186999ce49918d4fb61ea
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-23 12:55:13 -0700

    delete the test suite

--- cism.rb
+++ cism.rb
@@ -18,21 +18,3 @@ class Raindrops
 end
 
 
-class Raindrops
-
-  def convert(i)
-    plingplangplong.map do |factor, sound|
-        sound if i % factor == 0
-      end
-    plingplangplong.any? ? plingplangplong.join : i.to_s
-  end
-
-  def plingplangplong
-    %w(
-         'Pling', 
-       'Plang',
-         'Plong'
-         )
-  end
-
-end
