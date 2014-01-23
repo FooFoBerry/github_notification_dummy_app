370a9194a8378656ee9aab5994b4001aacf1be9d
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-23 12:58:57 -0700

    remove the file for demo purposes

--- cism.rb
+++ cism.rb
@@ -1,20 +1,3 @@
-class Raindrops
 
-  def convert(i)
-    plingplangplong.map do |factor, sound|
-        sound if i % factor == 0
-      end
-    plingplangplong.any? ? plingplangplong.join : i.to_s
-  end
-
-  def plingplangplong
-    %w(
-         'Pling', 
-       'Plang',
-         'Plong'
-         )
-  end
-
-end
 
 
